ebd5cf389f5328936be7a9340383aaf2bb7c5101
mutoch <89289398+mutoch@users.noreply.github.com>
2022-06-19 15:37:59 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "react-dom";
+
+class Human extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "Nakanishi" };
+  }
+
+  render() {
+    return (
+      <h2 onClick={this.onButtonClick}>
+        {this.state.name} {this.props.age}
+      </h2>
+    );
+  }
+
+  onButtonClick = () => {
+    this.setState({ name: this.state.name + "さん" });
+  };
+}
+
+render(<Human age="30" />, document.getElementById("root"));
