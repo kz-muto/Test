152ce7db106e711d9a93d1890505b043c7a08895
mutoch <89289398+mutoch@users.noreply.github.com>
2022-06-19 16:09:56 +0900

    ReactCompoinent-Class classMake

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
 import React from "react";
 import { render } from "react-dom";
-
-class Human extends React.Component {
+import "./App.css";
+class H2O extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "Nakanishi" };
+    this.state = { temp: 15 };
+  }
+
+  GetH2OState(temp) {
+    if (temp <= 0) {
+      return "ice";
+    }
+    if (temp >= 100) {
+      return "steam";
+    }
+    return "water";
   }
 
   render() {
+    const { temp } = this.state;
     return (
-      <h2 onClick={this.onButtonClick}>
-        {this.state.name} {this.props.age}
-      </h2>
+      <div className={this.GetH2OState(temp)}>
+        <h2>
+          phase:{this.GetH2OState(temp)} {temp}℃
+        </h2>
+        <button onClick={this.onPlusClick}>+</button>
+        <button onClick={this.onPlus10Click}>+10</button>
+        <button onClick={this.onMinusClick}>-</button>
+        <button onClick={this.onMinus10Click}>-10</button>
+      </div>
     );
   }
 
-  onButtonClick = () => {
-    this.setState({ name: this.state.name + "さん" });
+  onPlusClick = () => {
+    const { temp } = this.state;
+    this.setState({ temp: temp + 1 });
+  };
+  onPlus10Click = () => {
+    const { temp } = this.state;
+    this.setState({ temp: temp + 10 });
+  };
+  onMinusClick = () => {
+    const { temp } = this.state;
+
+    this.setState({ temp: temp - 1 });
+  };
+  onMinus10Click = () => {
+    const { temp } = this.state;
+    this.setState({ temp: temp - 10 });
   };
 }
 
-render(<Human age="30" />, document.getElementById("root"));
+render(<H2O />, document.getElementById("root"));
